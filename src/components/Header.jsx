ee4c897043142e5e906495d7ecc16060e3b98b73
Alexandre Oliveira <alexandreoliveira@Alexandres-MacBook-Pro.local>
2020-09-14 11:32:36 -0400

    Implemented components Chaeckout, Subtotal, and were installed packages react-currency-format, react-router-dom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
 
 
 
 function Header() {
     return (
         <div className="header">
-            <img
-                className="header__logo"
-                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-                alt="amazon_logo"
-            />
+
+            <Link to="">
+                <img
+                    className="header__logo"
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                    alt="amazon_logo"
+                />
+            </Link>
 
             <div className="header__search">
                 <input
@@ -37,11 +41,12 @@ function Header() {
                     <span className="header__optionLineTwo">Prime</span>
                 </div>
 
-                <div className="header__optionBasket">
-                    <ShoppingBasketIcon />
-                    <span className="header__optionLineTwo header__basketCount">0</span>
-                </div>
-
+                <Link to="/checkout">
+                    <div className="header__optionBasket">
+                        <ShoppingBasketIcon />
+                        <span className="header__optionLineTwo header__basketCount">0</span>
+                    </div>
+                </Link>
             </div>
         </div>
     )

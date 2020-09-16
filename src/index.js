206eb9e75aa6f5b91d37c7c1aa14f66db9832783
Alexandre Oliveira <alexandreoliveira@Alexandres-MacBook-Pro.local>
2020-09-16 18:34:56 -0400

    Implemented the basket to receive any product when the button Add to basket is clicked

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { StateProvider } from "./components/StateProvider";
+import reducer, { initialState } from "./components/Reducer";
 
-ReactDOM.render( < App / > , document.getElementById("root"));
+ReactDOM.render(
+    <React.StrictMode>
+        <StateProvider initialState={initialState} reducer={reducer}>
+            <App />
+        </StateProvider>
+    </React.StrictMode>
+    , document.getElementById("root"));
+
+// ServiceWorker.unregister();

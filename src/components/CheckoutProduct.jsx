35fcce1181e5e20054e3256f77c83505097d64b1
Alexandre Oliveira <alexandreoliveira@Alexandres-MacBook-Pro.local>
2020-10-08 16:17:07 -0400

    Implemented the function Remove from Basket on the component Reducer

--- src/components/CheckoutProduct.jsx
+++ src/components/CheckoutProduct.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({ id, image, title, price, rating }) {
+
+    const [{ basket }, dispatch] = useStateValue();
+
+    const removeFromBasket = () => {
+        dispatch({
+            type: "REMOVE_FROM_BASKET",
+            id: id,
+        })
+    }
+
     return (
         <div className="checkoutProduct">
             <img className="checkoutProduct__image" src={image} />
@@ -21,7 +32,7 @@ function CheckoutProduct({ id, image, title, price, rating }) {
                             <span role="img" aria-label="star">🌟</span>
                         ))}
                 </div>
-                <button>Remove from Basket</button>
+                <button onClick={removeFromBasket}>Remove from Basket</button>
             </div>
         </div>
     )
